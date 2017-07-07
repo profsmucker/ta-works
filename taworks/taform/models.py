1d201ae104560094e5d21ec2a1aecb28bf65a89e
Johnson Kan <j2kan@uwaterloo.ca>
2017-07-06 23:00:57 -0400

    can insert into student table

--- taworks/taform/models.py
+++ taworks/taform/models.py
@@ -2,14 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.core.validators import MaxLengthValidator
-from django.core.validators import MinLengthValidator
+from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
 import datetime
 from django.forms import ModelForm
 
 # Create your models here.
 class Student(models.Model):
-    student_id = models.PositiveIntegerField(validators=[MaxLengthValidator(8), MinLengthValidator(8)])
+    student_id = models.PositiveIntegerField(validators=[MaxValueValidator(99999999), MinValueValidator(10000000)])
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     quest_id = models.CharField(max_length=50)
@@ -17,28 +17,28 @@ class Student(models.Model):
     current_program = models.CharField(max_length=50)
     citizenship = models.BooleanField(default=False, blank=True)
     student_visa = models.BooleanField(default=False, blank=True)
-    student_visa_expiry_date = models.DateField(blank=True)
+    student_visa_expiry_date = models.DateField(null=True, blank=True)
     full_time = models.CharField(max_length=10)
     part_time = models.CharField(max_length=10)
     other = models.CharField(max_length=10)
-    cv = models.FileField(upload_to='documents/', blank=True)
-    past_position_one = models.CharField(max_length=255, blank=True)
-    past_position_two = models.CharField(max_length=255, blank=True)
-    past_position_three = models.CharField(max_length=255, blank=True)
+    cv = models.FileField(upload_to='documents/', null=True, blank=True)
+    past_position_one = models.CharField(max_length=255, null=True, blank=True)
+    past_position_two = models.CharField(max_length=255, null=True, blank=True)
+    past_position_three = models.CharField(max_length=255, null=True, blank=True)
 
 class Course(models.Model):
-    term = models.PositiveIntegerField(validators=[MaxLengthValidator(4), MinLengthValidator(4)])
-    section = models.PositiveIntegerField(validators=[MaxLengthValidator(3), MinLengthValidator(3)])
+    term = models.PositiveIntegerField(validators=[MaxValueValidator(9999), MinValueValidator(1000)])
+    section = models.PositiveIntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
     course_id = models.CharField(max_length=8)
     course_name = models.CharField(max_length=255)
     instructor_name = models.CharField(max_length=255)
     instructor_email = models.CharField(max_length=255)
+    preference = models.IntegerField(null=True, validators=[MaxValueValidator(3), MinValueValidator(-1)], blank=True)
+    reason = models.CharField(max_length=255, null=True, blank=True)
 
 class Application(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE, blank=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True)
-    preference = models.IntegerField(validators=[MaxLengthValidator(1), MinLengthValidator(1)], blank=True)
-    reason = models.CharField(max_length=255, blank=True)
     application_date = str(datetime.datetime.now())
 
 class StudentForm(ModelForm):
@@ -47,6 +47,12 @@ class StudentForm(ModelForm):
         fields = '__all__'
 
 class CourseForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CourseForm, self).__init__(*args, **kwargs)
+        uneditable_fields = ['term', 'section', 'course_id', 'course_name', 'instructor_name', 'instructor_email']
+        for field in uneditable_fields:
+            self.fields[field].widget.attrs['readonly'] = 'true'
+
     class Meta:
         model = Course
         fields = '__all__'
@@ -55,5 +61,4 @@ class ApplicationForm(ModelForm):
     class Meta:
         model = Application
         fields = '__all__'
-
 
--- taworks/taform/views.py
+++ taworks/taform/views.py
@@ -7,10 +7,24 @@ from . import models
 from django.urls import reverse
 # Create your views here.
 
-def index(request):
-    context = {
-        'student_form' : models.StudentForm(),
-        'course_form' : models.Course.objects.using('mysql').all(),
-        'application_form' : models.ApplicationForm()
-    }
-    return render(request, 'taform/application.html', context)
+def apply(request):
+    if request.method == 'POST':
+        s_form = models.StudentForm(request.POST, instance=models.Student())
+        c_forms = [models.CourseForm(request.POST, prefix=str(x), instance=x) for x in models.Course.objects.all()]
+        if s_form.is_valid():
+            s_form.save()
+        else:
+            print s_form
+            print "not valid"
+        # if all([cf.is_valid() for cf in c_forms]):
+        #     for cf in c_forms:
+        #         app = models.ApplicationForm(request.POST)
+        #         continue
+        return HttpResponse("done")
+    else:
+        c_forms = [models.CourseForm(prefix=str(x), instance=x) for x in models.Course.objects.all()]
+        context = {
+            's_form' : models.StudentForm(instance=models.Student()),
+            'c_forms' : c_forms
+            }
+        return render(request, 'taform/application.html', context)
