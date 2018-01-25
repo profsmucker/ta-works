642c14cbdddbc68f6c2543c450522eb9637a7103
smwatts <smwatts@uwaterloo.ca>
2018-01-24 22:57:15 -0500

    Updated checkbox logic, enabled jQuery

---
+++ taworks/taform/static/taform/js/app-jquery.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $( ".number" ).on( "click", function() {
+      if($( ".number:checked" ).length > 3)
+      {
+        $('#btn').prop('disabled', false);
+      }
+      else
+      {
+        $('#btn').prop('disabled', true);
+      }  
+    });
+
+});
