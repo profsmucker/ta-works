825bd8b831c98f8360bf90ddde16211a56ec6cd8
Johnson Kan <j2kan@uwaterloo.ca>
2018-02-20 10:16:35 -0500

    address feedbacks

--- taworks/taworks/urls.py
+++ taworks/taworks/urls.py
@@ -20,6 +20,5 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^taform/', include('taform.urls')),
-    url(r'^admin/', admin.site.urls),
     url(r'^favicon.ico/$', lambda x: HttpResponseRedirect(settings.STATIC_URL+'favicon/favicon.ico'))
 ]
